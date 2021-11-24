a04de027414b3d2e293db42cf22267f1e191e09e
Gabriel De Los Rios <gab.delosrios@gmail.com>
2021-11-24 10:18:50 -0300

    pass min len bug issue

--- routes/user.js
+++ routes/user.js
@@ -11,7 +11,7 @@ router.post('/',
         check('name', 'Name can not be empty').not().isEmpty(),
         check('lastname', 'Lastname can not be empty').not().isEmpty(),
         check('email', 'Email can not be empty').not().isEmpty(),
-        check('password', 'Password must be at least 6 characters long').not().isEmpty().isLength({options: { min:6 }})
+        check('password', 'Password must be at least 6 characters long').isLength({min:6})
     ],
     [
         check('email', 'Email must be a valid email address').isEmail(),
